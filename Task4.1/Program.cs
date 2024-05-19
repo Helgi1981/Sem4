392da03d431a7b3191a775d09fa67cfa5ddacddc
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-19 15:35:59 +0300

    Add Sem4:Task4.1

---
+++ Task4.1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Без использования функций!
+
+// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого чётная.
+
+while (true)
+{
+    Console.Write("Введите целое число (или 'q' для выхода): ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    bool isNumber = int.TryParse(input, out int number);
+    if (isNumber)
+    {
+        int sumOfDigits = 0;
+        foreach (char digit in input)
+        {
+            if (char.IsDigit(digit))
+            {
+                sumOfDigits += digit - '0';
+            }
+        }
+
+        if (sumOfDigits % 2 == 0)
+        {
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Пожалуйста, введите корректное целое число.");
+    }
+}
+
+Console.WriteLine("Программа завершена.");
