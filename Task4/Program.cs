e05a7cd8760aa53e997d4d9b27af5d6ba8c38643
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-19 15:23:50 +0300

    Add Sem4:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,65 @@
+﻿// Необходимо разделить логику алгоритмов на функции
+
+// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого чётная.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Run();
+    }
+
+    static void Run()
+    {
+        while (true)
+        {
+            string input = GetInput();
+
+            if (IsExitCommand(input))
+                break;
+
+            if (int.TryParse(input, out int number))
+            {
+                if (IsSumOfDigitsEven(number))
+                    break;
+            }
+        }
+
+        Console.WriteLine("Программа завершена.");
+    }
+
+    static string GetInput()
+    {
+        Console.Write("Введите целое число или 'q' для выхода: ");
+        return Console.ReadLine();
+    }
+
+    static bool IsExitCommand(string input)
+    {
+        return input.Equals("q", StringComparison.OrdinalIgnoreCase);
+    }
+
+    static bool IsSumOfDigitsEven(int number)
+    {
+        int sumOfDigits = CalculateSumOfDigits(number);
+        return sumOfDigits % 2 == 0;
+    }
+
+    static int CalculateSumOfDigits(int number)
+    {
+        int sum = 0;
+        number = Math.Abs(number); // Обработка отрицательных чисел
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
